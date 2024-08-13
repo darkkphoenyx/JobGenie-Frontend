5bd991f7b0d30eb5880ec1ed8c76511cdcb7898a
Deepesh Sunuwar <deepgeneral33@gmail.com>
2024-08-13 16:17:30 +0545

    Footer section problem fixed

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,46 +7,56 @@ export default function Footer() {
       <FooterNav />
       <div className=" pt-10">
         <div className="container mx-auto py-5 bg-gray-100/70 grid-cols-2 flex justify-between items-center px-10 rounded-2xl">
-          <div className="company-location-section">
-            <div className="logo_section flex">
-              <img
-                className="object-fill h-12 w-12"
-                src="/assets/company-logo.png"
-                alt="company logo"
-              />
-              <Link to={"/"}>
-                <div className="company-name">
-                  <h1 className="font-semibold  pl-2">Job Genie</h1>
-                  <p className="text-xs pl-2 text-gray-400">
-                    The Ultimate for Job Seekers
-                  </p>
-                </div>
-              </Link>
-            </div>
-            <div className="company-address text-gray-400 mt-3">
-              <h1 className="text-gray-500 text-sm">Mitrapark-10, Chabahil</h1>
-              <h1 className="text-gray-500 text-sm">Kathmandu, Nepal</h1>
-            </div>
-          </div>
-          <div className="newsletter-section w-2/5 pl-16">
-            <h1 className="text-3xl font-medium">Newsletter</h1>
-            <p className="text-gray-500 text-sm">
-              Subscribe to our newletter to get the latest Job Genie news
-            </p>
-            <div className="flex mt-3">
-              <div className="flex bg-white border border-gray-300 rounded-3xl px-4 w-[300px] mr-3">
-                <img className="pr-2" src="/assets/mail.svg" alt="mail icon" />
-                <input
-                  className="outline-none text-sm bg-white py-3 w-full"
-                  type="email"
-                  placeholder="Whats your work email?"
+          <div className="footer-start grid-rows-2 w-full">
+            <div className="top-row flex justify-between">
+              <div className="company-section flex">
+                <img
+                  className="object-fill h-12 w-12"
+                  src="/assets/company-logo.png"
+                  alt="company logo"
                 />
+                <Link to={"/"}>
+                  <div className="company-name">
+                    <h1 className="font-semibold pl-2">Job Genie</h1>
+                    <p className="text-xs pl-2 text-gray-400">
+                      The Ultimate for Job Seekers
+                    </p>
+                  </div>
+                </Link>
               </div>
-              <Link to={"#"}>
-                <button className="hover:text-[--color-text] hover:bg-white border hover:border-[--color-text] text-white bg-[--color-text] px-3 py-2 rounded-3xl">
-                  Subscribe
-                </button>
-              </Link>
+              <div className="Newsletter-section">
+                <h1 className="text-3xl font-medium">Newsletter</h1>
+                <p className="text-gray-500 text-sm">
+                  Subscribe to our newletter to get the latest Job Genie news
+                </p>
+              </div>
+            </div>
+            <div className="bottom-row flex justify-between items-center">
+              <div className="company-address text-gray-400">
+                <h1 className="text-gray-500 text-sm">
+                  Mitrapark-10, Chabahil
+                </h1>
+                <h1 className="text-gray-500 text-sm">Kathmandu, Nepal</h1>
+              </div>
+              <div className="flex items-center justify-center">
+                <div className="mt-1 send-email-section flex bg-white border border-gray-300 rounded-3xl px-4 w-[300px] mr-3">
+                  <img
+                    className="pr-2"
+                    src="/assets/mail.svg"
+                    alt="mail icon"
+                  />
+                  <input
+                    className="outline-none text-sm bg-white py-3 w-full"
+                    type="email"
+                    placeholder="Whats your work email?"
+                  />
+                </div>
+                <a href="#">
+                  <button className="hover:text-[--color-text] hover:bg-white border hover:border-[--color-text] text-white bg-[--color-text] px-3 py-2 rounded-3xl">
+                    Subscribe
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
         </div>
