35178430ffacb469fc7ba79bb5c3cf196815847b
Deepesh Sunuwar <deepgeneral33@gmail.com>
2024-08-30 10:11:53 +0545

    hamburger menu making part 1

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,88 +1,119 @@
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar() {
   const location = useLocation();
+  const [visible, setVisible] = useState(false);
 
   return (
-    <div className="shadow-md sticky top-0 bg-white/90 z-50">
-      <div className="navbar flex items-center h-[12vh] justify-between px-3 container mx-auto">
-        <div className="logo_section flex items-center space-x-2">
-          <img
-            className="object-fill h-12 w-12"
-            src="/assets/company-logo.webp"
-            alt="company logo"
-          />
-          <Link to={"/"}>
-            <div className="company-name text-xs sm:text-base">
-              <h1 className="font-semibold">Job Genie</h1>
-              <p className="text-xs text-gray-400">
-                The Ultimate for Job Seekers
-              </p>
-            </div>
-          </Link>
+    <div className="shadow-md w-full sticky top-0 bg-white/90 z-50 flex items-center justify-between px-5 gap-5">
+      <div className="logo_section flex items-center space-x-2">
+        <img
+          className="object-fill h-12 w-12"
+          src="/assets/company-logo.webp"
+          alt="company logo"
+        />
+        <Link to={"/"}>
+          <div className="company-name text-xs sm:text-base">
+            <h1 className="font-semibold">Job Genie</h1>
+            <p className="text-xs text-gray-400">
+              The Ultimate for Job Seekers
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div className="navbar sm:block hidden ">
+        <div className="flex items-center h-[12vh] justify-between px-3 container mx-auto">
+          <div className="nav-main h-max font-medium">
+            <ul className="flex gap-12 items-center my-9">
+              <li className="h-6">
+                <Link to={"/"}>
+                  {" "}
+                  {location.pathname == "/" ? (
+                    <p className="border-b-2 border-[--color-text] text-[--color-text]">
+                      Find Job
+                    </p>
+                  ) : (
+                    "Find Job"
+                  )}
+                </Link>
+              </li>
+              <li className="h-6">
+                <Link to={"/recruiter"}>
+                  {location.pathname == "/recruiter" ? (
+                    <p className="border-b-2 border-[--color-text] text-[--color-text]">
+                      For Recruiters
+                    </p>
+                  ) : (
+                    "For Recruiters"
+                  )}
+                </Link>
+              </li>
+              <li className="h-6">
+                <Link to={"/blogs"}>
+                  {location.pathname == "/blogs" ? (
+                    <p className="border-b-2 border-[--color-text] text-[--color-text]">
+                      Blogs
+                    </p>
+                  ) : (
+                    "Blogs"
+                  )}
+                </Link>
+              </li>
+              <li className="h-6">
+                <Link to={"/careertips"}>
+                  {location.pathname == "/careertips" ? (
+                    <p className="border-b-2 border-[--color-text] text-[--color-text]">
+                      Career tips
+                    </p>
+                  ) : (
+                    "Career tips"
+                  )}
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="nav-login">
+            <Link to={"/login"}>
+              <button className="px-4 mr-2 font-semibold transition-all rounded-3xl py-2 hover:bg-gray-400/70">
+                Login
+              </button>
+            </Link>
+            <Link to={"/register"}>
+              <button className="hover:text-white transition-all hover:bg-[--color-text] px-5 border py-2 rounded-3xl text-[--color-text] font-semibold border-[--color-text]">
+                Register Now
+              </button>
+            </Link>
+          </div>
         </div>
-        <div className="nav-main h-max font-medium">
-          <ul className="flex gap-12 items-center my-9">
-            <li className="h-6">
-              <Link to={"/"}>
-                {" "}
-                {location.pathname == "/" ? (
-                  <p className="border-b-2 border-[--color-text] text-[--color-text]">
-                    Find Job
-                  </p>
-                ) : (
-                  "Find Job"
-                )}
-              </Link>
+      </div>
+      <div className="block sm:hidden hamburger-menu">
+        <button onClick={() => setVisible(!visible)}>
+          <div>
+            <img src="/assets/menu.svg" alt="hamburger menu" />
+          </div>
+        </button>
+      </div>
+      {visible ? (
+        <div className="menu">
+          <ul>
+            <li>
+              <Link to={"/"}>Find job</Link>
             </li>
-            <li className="h-6">
-              <Link to={"/recruiter"}>
-                {location.pathname == "/recruiter" ? (
-                  <p className="border-b-2 border-[--color-text] text-[--color-text]">
-                    For Recruiters
-                  </p>
-                ) : (
-                  "For Recruiters"
-                )}
-              </Link>
+            <li>
+              <Link to={"/recruiter"}>For Recruiters</Link>
             </li>
-            <li className="h-6">
-              <Link to={"/blogs"}>
-                {location.pathname == "/blogs" ? (
-                  <p className="border-b-2 border-[--color-text] text-[--color-text]">
-                    Blogs
-                  </p>
-                ) : (
-                  "Blogs"
-                )}
-              </Link>
+            <li>
+              <Link to={"/blogs"}>Blogs</Link>
             </li>
-            <li className="h-6">
-              <Link to={"/careertips"}>
-                {location.pathname == "/careertips" ? (
-                  <p className="border-b-2 border-[--color-text] text-[--color-text]">
-                    Career tips
-                  </p>
-                ) : (
-                  "Career tips"
-                )}
-              </Link>
+            <li>
+              <Link to={"/careertips"}>Career tips</Link>
             </li>
           </ul>
         </div>
-        <div className="nav-login">
-          <Link to={"/login"}>
-            <button className="px-4 mr-2 font-semibold transition-all rounded-3xl py-2 hover:bg-gray-400/70">
-              Login
-            </button>
-          </Link>
-          <Link to={"/register"}>
-            <button className="hover:text-white transition-all hover:bg-[--color-text] px-5 border py-2 rounded-3xl text-[--color-text] font-semibold border-[--color-text]">
-              Register Now
-            </button>
-          </Link>
-        </div>
-      </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
